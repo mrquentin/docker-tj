b91cf1a32a4264aa2934e6d1fdfcc26aa4e9d588
MrQuentin <32539717+mrquentin@users.noreply.github.com>
2024-02-21 23:34:33 -0500

    Update script

--- launch.sh
+++ launch.sh
@@ -4,8 +4,8 @@ set -x
 
 cd /data
 
-minecraft_version=$(jq -r '.minecraft.version' /tmp-data/manifest.json)
-forge_version="${minecraft_version}-$(jq -r '.minecraft.modLoaders[] | select(.id | contains("forge")) | .id' /tmp-data/manifest.json | awk -F"-" '{print $2}')"
+minecraft_version=""
+forge_version=""
 
 if ! [[ "$EULA" = "false" ]]; then
   echo "eula=true" > eula.txt
@@ -15,10 +15,12 @@ else
 fi
 
 if ! [[ -f "Server-Files-$PROJECT_VERSION.zip" ]]; then
-  rm -fr rm -fr config defaultconfigs resources libraries kubejs scripts mods packmenu Simple.zip forge*
+  rm -fr rm -fr config defaultconfigs resources libraries kubejs scripts overrides mods packmenu Simple.zip forge* manifest.json
   curl -Lo "Server-Files-$PROJECT_VERSION.zip" "$(jq -r '.downloadUrl' /pack-info.json)" || exit 9
-  unzip -u -o "Server-Files-$PROJECT_VERSION.zip" -d /tmp-data
-  mv -r /tmp-data/overrides/* /data/ # extract pack data
+  unzip -u -o "Server-Files-$PROJECT_VERSION.zip" -d /data/tmp
+  mv -r /data/tmp/overrides/* /data/ # extract pack data
+  minecraft_version=$(jq -r '.minecraft.version' /data/tmp/manifest.json)
+  forge_version="${minecraft_version}-$(jq -r '.minecraft.modLoaders[] | select(.id | contains("forge")) | .id' /data/manifest.json | awk -F"-" '{print $2}')"
   curl -Lo "forge-${forge_version}-installer.jar" "http://files.minecraftforge.net/maven/net/minecraftforge/forge/${forge_version}/forge-${forge_version}-installer.jar"
   java -jar "forge-${forge_version}-installer.jar" --installServer
 fi
