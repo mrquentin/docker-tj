213914a2a1ef9b0e8047302c82b0b00075ff065e
MrQuentin <32539717+mrquentin@users.noreply.github.com>
2024-02-21 23:12:38 -0500

    Update script

--- launch.sh
+++ launch.sh
@@ -27,6 +27,4 @@ if [[ -n "$JVM_OPTS" ]]; then
 	for j in ${JVM_OPTS}; do sed -i '$a\'$j'' user_jvm_args.txt; done
 fi
 
-chmod 755 run.sh
-
 java -jar @user_jvm_args.txt "forge-${forge_version}.jar" --nogui
