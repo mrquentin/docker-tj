616dc368f82709a2930956eb88456300626847e8
MrQuentin <32539717+mrquentin@users.noreply.github.com>
2024-02-21 23:39:10 -0500

    Fix

--- launch.sh
+++ launch.sh
@@ -15,12 +15,12 @@ else
 fi
 
 if ! [[ -f "Server-Files-$PROJECT_VERSION.zip" ]]; then
-  rm -fr rm -fr config defaultconfigs resources libraries kubejs scripts overrides mods packmenu Simple.zip forge* manifest.json
+  rm -fr rm -fr config defaultconfigs resources libraries kubejs scripts tmp mods packmenu Server*.zip forge*
   curl -Lo "Server-Files-$PROJECT_VERSION.zip" "$(jq -r '.downloadUrl' /pack-info.json)" || exit 9
   unzip -u -o "Server-Files-$PROJECT_VERSION.zip" -d /data/tmp
   mv -r /data/tmp/overrides/* /data/ # extract pack data
   minecraft_version=$(jq -r '.minecraft.version' /data/tmp/manifest.json)
-  forge_version="${minecraft_version}-$(jq -r '.minecraft.modLoaders[] | select(.id | contains("forge")) | .id' /data/manifest.json | awk -F"-" '{print $2}')"
+  forge_version="${minecraft_version}-$(jq -r '.minecraft.modLoaders[] | select(.id | contains("forge")) | .id' /data/tmp/manifest.json | awk -F"-" '{print $2}')"
   curl -Lo "forge-${forge_version}-installer.jar" "http://files.minecraftforge.net/maven/net/minecraftforge/forge/${forge_version}/forge-${forge_version}-installer.jar"
   java -jar "forge-${forge_version}-installer.jar" --installServer
 fi
