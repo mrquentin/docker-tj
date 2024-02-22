6c83e147ac781d8022b0c0c38b7b4b1e76b30a0a
MrQuentin <32539717+mrquentin@users.noreply.github.com>
2024-02-21 22:51:51 -0500

    Create Dockerfile and scripts

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM openjdk:8-buster
+
+ARG PROJECT_VERSION="1.3"
+
+LABEL version=$PROJECT_VERSION
+
+RUN apt-get update && apt-get install -y curl unzip jq && \
+ adduser --uid 99 --gid 100 --home /data --disabled-password minecraft
+
+COPY launch.sh /launch.sh
+COPY server-file-info.json /server-file-info.json
+COPY user_jvm_args.txt /data/user_jvm_args.txt
+RUN chmod +x /launch.sh
+
+USER minecraft
+
+ENV PROJECT_VERSION=$PROJECT_VERSION
+
+VOLUME /data
+WORKDIR /data
+
+EXPOSE 25565/tcp
+
+CMD ["/launch.sh"]
---
+++ launch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+
+cd /data
+
+if ! [[ "$EULA" = "false" ]]; then
+  echo "eula=true" > eula.txt
+else
+  echo "You must accept the EULA to install"
+  exit 99
+fi
+
+if ! [[ -f "Server-Files-$PROJECT_VERSION.zip" ]]; then
+  rm -fr rm -fr config defaultconfigs resources libraries kubejs scripts mods packmenu Simple.zip forge*
+  curl -Lo "Server-Files-$PROJECT_VERSION.zip" "$(jq -r '.downloadUrl' /pack-info.json)" || exit 9
+  unzip -u -o "Server-Files-$PROJECT_VERSION.zip" -d /tmp-data
+  mv -r /tmp-data/overrides/* /data/ # extract pack data
+  minecraft_version=$(jq -r '.minecraft.version' /tmp-data/manifest.json)
+  forge_version="${minecraft_version}-$(jq -r '.minecraft.modLoaders[] | select(.id | contains("forge")) | .id' /tmp-data/manifest.json | awk -F"-" '{print $2}')"
+  curl -Lo "forge-${forge_version}-installer.jar" "http://files.minecraftforge.net/maven/net/minecraftforge/forge/${forge_version}/forge-${forge_version}-installer.jar"
+  java -jar "forge-${forge_version}-installer.jar" --installServer
+fi
+
+if [[ -n "$JVM_OPTS" ]]; then
+	sed -i '/-Xm[s,x]/d' user_jvm_args.txt
+	for j in ${JVM_OPTS}; do sed -i '$a\'$j'' user_jvm_args.txt; done
+fi
+
+chmod 755 run.sh
+
+java -jar @user_jvm_args.txt "forge-${forge_version}.jar" --nogui
