cdc8ca6a005fd2ce42f337945a447a5948674a12
MrQuentin <32539717+mrquentin@users.noreply.github.com>
2024-02-21 23:24:13 -0500

    Update script

--- launch.sh
+++ launch.sh
@@ -4,6 +4,9 @@ set -x
 
 cd /data
 
+minecraft_version=$(jq -r '.minecraft.version' /tmp-data/manifest.json)
+forge_version="${minecraft_version}-$(jq -r '.minecraft.modLoaders[] | select(.id | contains("forge")) | .id' /tmp-data/manifest.json | awk -F"-" '{print $2}')"
+
 if ! [[ "$EULA" = "false" ]]; then
   echo "eula=true" > eula.txt
 else
@@ -16,8 +19,6 @@ if ! [[ -f "Server-Files-$PROJECT_VERSION.zip" ]]; then
   curl -Lo "Server-Files-$PROJECT_VERSION.zip" "$(jq -r '.downloadUrl' /pack-info.json)" || exit 9
   unzip -u -o "Server-Files-$PROJECT_VERSION.zip" -d /tmp-data
   mv -r /tmp-data/overrides/* /data/ # extract pack data
-  minecraft_version=$(jq -r '.minecraft.version' /tmp-data/manifest.json)
-  forge_version="${minecraft_version}-$(jq -r '.minecraft.modLoaders[] | select(.id | contains("forge")) | .id' /tmp-data/manifest.json | awk -F"-" '{print $2}')"
   curl -Lo "forge-${forge_version}-installer.jar" "http://files.minecraftforge.net/maven/net/minecraftforge/forge/${forge_version}/forge-${forge_version}-installer.jar"
   java -jar "forge-${forge_version}-installer.jar" --installServer
 fi
